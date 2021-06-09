c819cae69662b5a228a0cbed4d973d01bf97deec
Julio Augusto <julioams@icloud.com>
2021-06-09 19:41:59 -0300

    feat: change interceptor errors

--- app/api/utils/interceptorErrors.ts
+++ app/api/utils/interceptorErrors.ts
@@ -5,27 +5,53 @@ export default {
     if (err instanceof TypeError) {
       return new errors.BadRequestError(err)
     } else {
-      const { code, message } = err.response.data
+      const { code, status } = err.response || err.response.data
 
-      if (code === 400) {
-        return new errors.BadRequestError(err)
+      console.log(err.response.data.errors)
+
+      if (code || status === 400) {
+        return new errors.BadRequestError(
+          { statusCode: 400 },
+          err.response.data.errors[0].message ||
+            'Algum parametro necessario não foi passado!'
+        )
       }
 
-      if (code === 401) {
-        return new errors.UnauthorizedError(err)
+      if (code || status === 401) {
+        return new errors.UnauthorizedError(
+          {
+            statusCode: 401,
+          },
+          'Você não tem permissão para acessar este serviço!'
+        )
       }
 
-      if (code === 403) {
-        return new errors.ForbiddenError(err)
+      if (code || status === 403) {
+        return new errors.ForbiddenError(
+          {
+            statusCode: 403,
+          },
+          'Você não tem autorização para acessar este serviço!'
+        )
       }
 
-      if (code === 404) {
-        return new errors.NotFoundError(message)
+      if (code || status === 404) {
+        return new errors.NotFoundError(
+          {
+            statusCode: 404,
+          },
+          'Não foi possivel encontrar o que você está solicitando!'
+        )
       }
 
       // declarações para manipular quaisquer exceções não especificadas
       // passa o objeto de exceção para o manipulador de erro
-      return new errors.InternalServerError(err)
+      return new errors.InternalServerError(
+        {
+          statusCode: 500,
+        },
+        'Ocorreu um erro interno de servidor, tente novamente ou entre em contato com o administrador do sistema!'
+      )
     }
   },
 }
